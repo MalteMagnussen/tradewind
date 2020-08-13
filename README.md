e3a562e0b90fbdb84ccf3f07cf74ea4836e33c20
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-08-13 20:02:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Tradewind
+ Tradewind Flutter Site
---
+++ android/app/src/main/kotlin/com/example/tradewind/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tradewind
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}

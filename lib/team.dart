438a64a4c445ecebc2658c007f80a05b8544868e
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2020-08-14 21:52:43 +0200

    Responsive Team Cards

--- lib/team.dart
+++ lib/team.dart
@@ -17,29 +17,61 @@ class Team extends StatelessWidget {
         title: Text('Team Tradewind'),
       ),
       body: Padding(
-        padding: const EdgeInsets.only(top: 50.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            TeamMember(
-              email: 'august_zachariae@hotmail.com',
-              image: 'https://i.imgur.com/FrcY2kd.jpg',
-              text:
-                  'Sustainable energy engineer educated at Technical University of Denmark. August began his career doing electricity price forecasting and economic feasibility studies in Copenhagen. August will use his skills in the field to make electricity price projections and production forecasts.',
-            ),
-            TeamMember(
-              email: 'stokholm@space.dtu',
-              text:
-                  'As an educated electrical engineer from the Technical University of Denmark working at the space institute at the university. Andreas has had plenty of experience working with big data from satellites.',
-              image: 'https://i.imgur.com/b9y2su1.jpg',
-            ),
-            TeamMember(
-              email: 'maltehviidmagnussen@gmail.com',
-              text:
-                  'As a top student at Copenhagen Business Academy studying Computer Science, Malte maintains Tradewinds’ IT-systems and websites.',
-              image: 'https://i.imgur.com/wuMvSe5.png',
-            )
-          ],
+        padding: const EdgeInsets.only(top: 30.0),
+        child: SafeArea(
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              if (constraints.maxWidth < 800) {
+                return ListView(
+                  padding: EdgeInsets.all(10.0),
+                  children: [
+                    TeamMember(
+                      email: 'august_zachariae@hotmail.com',
+                      image: 'https://i.imgur.com/FrcY2kd.jpg',
+                      text:
+                          'Sustainable energy engineer educated at Technical University of Denmark. August began his career doing electricity price forecasting and economic feasibility studies in Copenhagen. August will use his skills in the field to make electricity price projections and production forecasts.',
+                    ),
+                    TeamMember(
+                      email: 'stokholm@space.dtu',
+                      text:
+                          'As an educated electrical engineer from the Technical University of Denmark working at the space institute at the university. Andreas has had plenty of experience working with big data from satellites.',
+                      image: 'https://i.imgur.com/b9y2su1.jpg',
+                    ),
+                    TeamMember(
+                      email: 'maltehviidmagnussen@gmail.com',
+                      text:
+                          'As a top student at Copenhagen Business Academy studying Computer Science, Malte maintains Tradewinds’ IT-systems and websites.',
+                      image: 'https://i.imgur.com/wuMvSe5.png',
+                    )
+                  ],
+                );
+              } else {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    TeamMember(
+                      email: 'august_zachariae@hotmail.com',
+                      image: 'https://i.imgur.com/FrcY2kd.jpg',
+                      text:
+                          'Sustainable energy engineer educated at Technical University of Denmark. August began his career doing electricity price forecasting and economic feasibility studies in Copenhagen. August will use his skills in the field to make electricity price projections and production forecasts.',
+                    ),
+                    TeamMember(
+                      email: 'stokholm@space.dtu',
+                      text:
+                          'As an educated electrical engineer from the Technical University of Denmark working at the space institute at the university. Andreas has had plenty of experience working with big data from satellites.',
+                      image: 'https://i.imgur.com/b9y2su1.jpg',
+                    ),
+                    TeamMember(
+                      email: 'maltehviidmagnussen@gmail.com',
+                      text:
+                          'As a top student at Copenhagen Business Academy studying Computer Science, Malte maintains Tradewinds’ IT-systems and websites.',
+                      image: 'https://i.imgur.com/wuMvSe5.png',
+                    )
+                  ],
+                );
+              }
+            },
+          ),
         ),
       ),
     );
